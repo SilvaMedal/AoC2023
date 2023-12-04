be95e7c828cae8818b306624aea9ebc002e707e1
Anthony Silva <budsilva04@gmail.com>
2023-12-04 18:27:51 -0500

    Submit part 2

--- day3.js
+++ day3.js
@@ -3,9 +3,11 @@ import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", "utf-8").split("\n");
 
 const numReg = /\d/;
-const nonSymbol = /\d|\./;
-let partSum = 0;
+// const nonSymbol = /\d|\./;
+const gear = /\*/;
+let gearSum = 0;
 const pad = ".";
+const gearMap = new Map();
 
 // pads the input with '.', to create an invisible wall,
 // this nullifies the need for making edge-case exceptions.
@@ -22,63 +24,79 @@ for (let i = 0; i < paddedInput.length; i++) {
 [i+1,j-1] [i+1,j] [i+1,j+1]
 */
 
-// maybe look at noting the indices of numbers and symbols by line and
-// see where they lap/meet?
-
+// starts looping through the 'padded' 2D array
 for (let i = 1; i < paddedInput.length - 1; i++) {
   for (let j = 1; j < paddedInput[i].length - 1; j++) {
     let tempNum = "";
-    let partBool = false;
 
+    // if we have a number...
     if (numReg.test(paddedInput[i][j])) {
-      if (
-        !nonSymbol.test(paddedInput[i - 1][j - 1]) ||
-        !nonSymbol.test(paddedInput[i - 1][j]) ||
-        !nonSymbol.test(paddedInput[i - 1][j + 1]) ||
-        !nonSymbol.test(paddedInput[i][j - 1]) ||
-        !nonSymbol.test(paddedInput[i][j + 1]) ||
-        !nonSymbol.test(paddedInput[i + 1][j - 1]) ||
-        !nonSymbol.test(paddedInput[i + 1][j]) ||
-        !nonSymbol.test(paddedInput[i + 1][j + 1])
-      ) {
-        partBool = true;
-        tempNum += paddedInput[i][j];
-      }
-      // test numbers left of the number passing as a part
-      if (partBool && numReg.test(paddedInput[i][j - 1])) {
-        let prev = true;
-        let x = 1;
+      // ...check around the number for gears
+      for (let a = i - 1; a < i + 2; a++) {
+        for (let b = j - 1; b < j + 2; b++) {
+          let isGear = gear.test(paddedInput[a][b]);
+          // if we find a gear...
+          if (isGear) {
+            // ...start tracking the number
+            tempNum += paddedInput[i][j];
+            // test for numbers left of the number passing as a part
+            if (numReg.test(paddedInput[i][j - 1])) {
+              let prev = true;
+              let x = 1;
 
-        while (prev) {
-          let prevNum = paddedInput[i][j - x];
-          tempNum = prevNum + tempNum;
-          x++;
-          if (!numReg.test(paddedInput[i][j - x])) {
-            prev = false;
-          }
-        }
-      }
-      // test numbers right of the number passing as a part
-      if (partBool && numReg.test(paddedInput[i][j + 1])) {
-        let next = true;
-        let y = 1;
-        let jump = 0;
-        while (next) {
-          let nextNum = paddedInput[i][j + y];
-          tempNum = tempNum + nextNum;
-          y++;
-          jump++;
-          if (!numReg.test(paddedInput[i][j + y])) {
-            next = false;
-            j += jump;
+              while (prev) {
+                let prevNum = paddedInput[i][j - x];
+                tempNum = prevNum + tempNum;
+                x++;
+                if (!numReg.test(paddedInput[i][j - x])) {
+                  prev = false;
+                }
+              }
+            }
+            // test for numbers right of the number passing as a part
+            if (numReg.test(paddedInput[i][j + 1])) {
+              let next = true;
+              let y = 1;
+              let jump = 0;
+              while (next) {
+                let nextNum = paddedInput[i][j + y];
+                tempNum = tempNum + nextNum;
+                y++;
+                jump++;
+                if (!numReg.test(paddedInput[i][j + y])) {
+                  next = false;
+                  j += jump;
+                }
+              }
+            }
+            // console.log(tempNum, [a, b]);
+
+            // make a key using the gear coordinates as a string...
+            let key = `${a}+${b}`;
+            // ...then check if we have logged a part here yet.
+            if (!gearMap.get(key)) {
+              // if not, make an entry (with the value in an array)
+              gearMap.set(key, [Number(tempNum)]);
+            } else {
+              // if so, add to the gear's array
+              gearMap.get(key).push(Number(tempNum));
+            }
           }
         }
       }
     }
-    //
-    // console.log(tempNum);
-    partSum += Number(tempNum);
   }
 }
 
-console.log(partSum);
+// function parameters order is (value, key, map)
+// with the latter 2 not being needed here.
+function findTwoPartGears(value) {
+  if (value.length === 2) {
+    gearSum += value[0] * value[1];
+  }
+}
+// Map.prototype.forEach() executes a provided function once
+// per each key/value pair in this map, in insertion order (value, key, map);
+gearMap.forEach(findTwoPartGears);
+
+console.log(gearSum);
