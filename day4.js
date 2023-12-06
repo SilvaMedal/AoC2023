13bdacbb2a1fa711ec75d5e092e84adb483438cb
Anthony Silva <budsilva04@gmail.com>
2023-12-06 00:30:31 -0500

    Submit day4 part2

--- day4.js
+++ day4.js
@@ -1,23 +1,36 @@
 import { readFileSync } from "fs";
 
-const input = readFileSync("./example.txt", "utf-8").split("\n");
+const input = readFileSync("./input.txt", "utf-8").split("\n");
 
 const noCard = /\:(.*)/;
 const numReg = /\d+/g;
 let games = [];
-let points = 0;
 
 for (let i = 0; i < input.length; i++) {
   let string = input[i].match(noCard)[0];
   games.push(string.match(numReg));
 }
 
-for (let i = 0; i < games.length; i++) {
-  let wins = games[i].filter((x, index) => games[i].indexOf(x) !== index);
+// an array to track card count of each game
+const cardMap = Array(games.length).fill(1);
 
-  if (wins.length !== 0) {
-    points += 2 ** (wins.length - 1);
+// for each game...
+for (let i = 0; i < games.length; i++) {
+  let j = i;
+  // ...find the number of wins
+  let wins = [...games[i].filter((x, index) => games[i].indexOf(x) !== index)]
+    .length;
+  // find out how many cards of this game you have
+  let numCards = cardMap[i];
+  // use this loop to add cards for each win
+  while (wins > 0) {
+    cardMap[j + 1] += numCards;
+    console.log(cardMap);
+    j++;
+    wins--;
   }
 }
+// sum the total cards in array
+const cards = cardMap.reduce((a, b) => a + b);
 
-console.log(points);
+console.log(cards);
