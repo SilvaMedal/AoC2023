2a553fda3adea81c1230d094edcc49b39cd2da4e
Anthony Silva <budsilva04@gmail.com>
2023-12-09 00:33:43 -0500

    Submit day6 part2

--- day6.js
+++ day6.js
@@ -2,33 +2,23 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("./input.txt", "utf-8").split("\n");
 
-const times = input[0].replace("Time:", "").trim().match(/\d+/g).map(Number);
-const distances = input[1]
-  .replace("Distance:", "")
-  .trim()
-  .match(/\d+/g)
-  .map(Number);
+const time = Number(
+  input[0].replace("Time:", "").trim().match(/\d+/g).join("")
+);
+const distance = Number(
+  input[1].replace("Distance:", "").trim().match(/\d+/g).join("")
+);
+// console.log(time, distance);
 
-const recordPerDistance = [];
-const waysToWin = [];
+let ways = 0;
+let hold = 0;
 
-for (let i = 0; i < times.length; i++) {
-  recordPerDistance.push([times[i], distances[i]]);
+for (let i = 0; i < time; i++) {
+  let timeToTravel = time - hold;
+  if (timeToTravel * hold > distance) {
+    ways++;
+  }
+  hold++;
 }
 
-recordPerDistance.forEach((record) => {
-  let ways = 0;
-  let time = record[0];
-  let distance = record[1];
-  let hold = 0;
-  for (let i = 0; i < time; i++) {
-    let timeToTravel = time - hold;
-    if (timeToTravel * hold > distance) {
-      ways++;
-    }
-    hold++;
-  }
-  waysToWin.push(ways);
-});
-
-console.log(waysToWin.reduce((a, b) => a * b));
+console.log(ways);
