2f0d42027196ccb1c1aac878f03c94048d4395e2
Anthony Silva <budsilva04@gmail.com>
2023-12-21 16:18:10 -0600

    Submit day11 part2

--- day11.js
+++ day11.js
@@ -5,15 +5,21 @@ const input = readFileSync("./input.txt", "utf-8").split("\n");
 const grid = input.map((line) => line.split(""));
 const galaxyRegex = /\#/;
 let counter = 0;
+const spaceGap = 1000000;
 
 // test for empty rows
 const newRows = [];
+
+// Now, for gaps, instead of pushing further "empty" rows, we push a single
+// identifier row.  Later, we'll check for crossing one of these gaps, and
+// add it's quantity to the total.
+
 for (let i = 0; i < grid.length; i++) {
   if (galaxyRegex.test(input[i])) {
     newRows.push(grid[i]);
   } else {
-    newRows.push(grid[i]);
-    newRows.push(grid[i]);
+    let gap = new Array(grid[i].length).fill("x");
+    newRows.push(gap);
   }
 }
 const rotatedGrid = [];
@@ -22,15 +28,15 @@ for (let col = 0; col < newRows[0].length; col++) {
   for (let row = 0; row < newRows.length; row++) {
     rotatedGrid[col].unshift(newRows[row][col]);
   }
-  rotatedGrid[col] = rotatedGrid[col].join("");
+  rotatedGrid[col] = rotatedGrid[col];
 }
 const newGrid = [];
 for (let i = 0; i < rotatedGrid.length; i++) {
   if (galaxyRegex.test(rotatedGrid[i])) {
     newGrid.push(rotatedGrid[i]);
   } else {
-    newGrid.push(rotatedGrid[i]);
-    newGrid.push(rotatedGrid[i]);
+    let gap = new Array(rotatedGrid[i].length).fill("x");
+    newGrid.push(gap);
   }
 }
 const map = [];
@@ -42,17 +48,46 @@ for (let x = 0; x < newGrid.length; x++) {
     }
   }
 }
-let iterations = 1;
-let sub = 0;
 while (map.length > 1) {
   for (let i = 1; i < map.length; i++) {
-    let dist =
-      Math.abs(map[i][0] - map[0][0]) + Math.abs(map[i][1] - map[0][1]);
-    counter += dist;
-    sub++;
+    let x = map[i][0];
+    let y = map[i][1];
+    while (x !== map[0][0]) {
+      if (x > map[0][0]) {
+        x--;
+        if (newGrid[x][y] === "x") {
+          counter += spaceGap;
+        } else {
+          counter++;
+        }
+      } else {
+        x++;
+        if (newGrid[x][y] === "x") {
+          counter += spaceGap;
+        } else {
+          counter++;
+        }
+      }
+    }
+    while (y !== map[0][1]) {
+      if (y > map[0][1]) {
+        y--;
+        if (newGrid[x][y] === "x") {
+          counter += spaceGap;
+        } else {
+          counter++;
+        }
+      } else {
+        y++;
+        if (newGrid[x][y] === "x") {
+          counter += spaceGap;
+        } else {
+          counter++;
+        }
+      }
+    }
   }
   map.shift();
-  iterations++;
 }
 
 console.log(counter);
